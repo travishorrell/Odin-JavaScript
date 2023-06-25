9414da607186d233a98f9ff37414ef31e3fc8e73
travishorrell <17819559+travishorrell@users.noreply.github.com>
2023-06-24 19:10:41 -0600

    Created two pages and notes

---
+++ javaScript.js
@@ -0,0 +1,24 @@
+
+//Creating variables
+
+let message;
+message = "This is the message from our data varaible"; // store the message in out variable named "message"
+
+console.log(message); //We are printing the message variable to the console using the console.log method
+alert(message); //We are priting the messge varaible to the screen using the alert function
+
+//Creating new variables
+let user = 'John';
+let age = 25;
+let johnsMessage = 'hello';
+
+//See Note page "variableNotes.txt"
+
+//Creating const variables, variable that can not change
+
+const myBirthday = '04.16.1990';
+
+myBirthday = '12.25.1955';
+
+console.log(myBirthday);
+
